d4751b0d164465836c4ba0906441a045c34d592a
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2025-08-04 10:25:09 +0300

    simple init but hitting solana limits - reallocs are limited to 10k

--- programs/sring/src/lib.rs
+++ programs/sring/src/lib.rs
@@ -1,16 +1,54 @@
 use anchor_lang::prelude::*;
 
+mod tests;
+
 declare_id!("52rK34hDp4374vyuc2Psp2951rEpGwj7UpisGVeHjdWW");
+
+pub const FRAME_LEN: u64 = 1024;
+pub const FRAMES_NUM: u64 = 9;
+
+#[derive(InitSpace)]
+#[account]
+pub struct SRing {
+    pub current_idx: u64,
+    pub next_idx: u64,
+    #[max_len(FRAMES_NUM * FRAME_LEN)]
+    pub frames: Vec<[u8; FRAME_LEN as usize]>,
+}
 
 #[program]
 pub mod sring {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_ring(ctx: Context<InitializeRing>) -> Result<()> {
+        msg!("Greetings from: {:?} - initialize", ctx.program_id);
+        let ring_account = &mut ctx.accounts.ring_account;
+        ring_account.current_idx = 0;
+        ring_account.next_idx = 0;
+        for _ in 0..FRAMES_NUM {
+            ring_account.frames.push([0u8; FRAME_LEN as usize]);
+        }
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(bump: u8)]
+pub struct InitializeRing<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    // The PDA being created, initialized with the derived space including the discriminator(8).
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + SRing::INIT_SPACE,
+        seeds = [b"sring", owner.key.as_ref()],
+        bump,
+    )]
+    pub ring_account: Account<'info, SRing>,
+
+    // System program is required for account creation.
+    pub system_program: Program<'info, System>,
+}
---
+++ programs/sring/src/tests.rs
@@ -0,0 +1,72 @@
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use anchor_lang::prelude::borsh;
+    use litesvm::LiteSVM;
+    use solana_instruction::{AccountMeta, Instruction};
+    use solana_keypair::Keypair;
+    use solana_program::{entrypoint::ProgramResult, system_program};
+    use solana_pubkey::Pubkey;
+    use solana_signer::Signer;
+    use solana_transaction::Transaction;
+    use solana_transaction_error::TransactionError;
+
+    #[test]
+    fn test_initialize_ring() -> ProgramResult {
+        let mut svm = LiteSVM::new();
+        let payer_kp = Keypair::new();
+        let payer_pk = payer_kp.pubkey();
+        println!("Payer address: {}", payer_pk);
+        let program_id = Pubkey::from_str(&crate::ID.to_string()).unwrap();
+        println!("Program id: {}", program_id);
+        let bytes = include_bytes!("../../../target/deploy/sring.so");
+        svm.add_program(program_id.clone(), bytes);
+        // svm.with_coverage(
+        //     vec![(program_id, "native_app".into())],
+        //     vec![],
+        //     payer_kp.insecure_clone(),
+        // )
+        // .unwrap();
+
+        svm.airdrop(&payer_pk, 100000000000).unwrap();
+        let recent_blockhash = svm.latest_blockhash();
+
+        // Derive expected PDA and bump (example seeds)
+        let seeds = &[b"sring", payer_pk.as_ref()];
+        let (pda_pubkey, _bump) = Pubkey::find_program_address(seeds, &program_id);
+
+        let ix_accounts = vec![
+            // ORDER IS IMPORTANT!
+            AccountMeta::new(payer_pk, true),
+            AccountMeta::new(pda_pubkey, false), // even if not created already we must pass it!
+            AccountMeta::new(system_program::ID, false),
+        ];
+        // discriminator + a borshed empty vec....
+        let mut ix_data = vec![183, 129, 68, 92, 121, 234, 98, 108];
+        let ix_data = {
+            let v: Vec<u8> = vec![];
+            let data = borsh::to_vec(&v)?;
+            // println!("empty vec borshed -> {:?}", data);
+            ix_data.extend_from_slice(&data);
+            ix_data
+        };
+        let instructions = [Instruction::new_with_bytes(
+            program_id,
+            &ix_data,
+            ix_accounts,
+        )];
+
+        let trans = Transaction::new_signed_with_payer(
+            &instructions[..],
+            Some(&payer_pk),
+            &[&payer_kp],
+            recent_blockhash,
+        );
+
+        let res = svm.send_transaction(trans.clone()).unwrap();
+        println!("sring's initialize_ring -> {}", res.pretty_logs());
+
+        Ok(())
+    }
+}
